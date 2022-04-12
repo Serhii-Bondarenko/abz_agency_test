8716b1a0814e58e69028952536f016e0e257eb20
Serhii-Bondarenko <sergeybondd@gmail.com>
2022-04-12 23:55:11 +0300

    gh pages lib

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <Provider store={ store }>
-        <BrowserRouter>
+        <BrowserRouter  basename={ window.location.pathname || '' }>
             <App/>
         </BrowserRouter>
     </Provider>
