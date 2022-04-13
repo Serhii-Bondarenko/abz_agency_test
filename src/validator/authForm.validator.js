64218f99cba2d643da68a9c5e70569d6e1361456
Serhii-Bondarenko <sergeybondd@gmail.com>
2022-04-13 09:07:09 +0300

    fix email field validation (pattern)

--- src/validator/authForm.validator.js
+++ src/validator/authForm.validator.js
@@ -8,10 +8,12 @@ const authFormSchema = Joi.object({
 
     email: Joi.string().min(2).max(100).trim()
         .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ua'] } })
+        .regex(new RegExp('^(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])$'))
         .required()
         .messages({
             'string.empty': 'The email field is required.',
-            'string.email': 'The email must be a valid email address (user@email.com).'
+            'string.email': 'The email must be a valid email address (user@email.com).',
+            'string.pattern.base': 'The email format is invalid.'
         }),
 
     phone: Joi.string().min(13).trim().max(13).regex(new RegExp('^[+]?380([0-9]{9})$'))
